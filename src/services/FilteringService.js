d472a5120ea4c98cb42efbca7c69d99f6758e036
unknown <470994108@qq.com>
2019-10-20 16:11:06 +0200

    update function filterByAgerange

--- src/services/FilteringService.js
+++ src/services/FilteringService.js
@@ -1,19 +1,8 @@
 const ss = require('./SimilarityService');
 
-//on condition that we have information of user's age range
-//filter the users first by age range, then by the target user's owned securities
-exports.filteringForVoting = function(target, users){
+exports.filterByAgerange = function(target, users){
     var filter_by_agerange = users.filter(user => user.agerange == target.agerange);
-    var filter_by_target_owned = filter_by_agerange.filter(user => function isSubset(){
-        if(target.ownedSec.length > user.ownedSec.length) return false;
-        target.ownedSec.forEach(elem => {
-            if(user.ownedSec.findIndex(function(obj){
-                return elem.isin == obj.isin;
-            }) == -1) return false;
-        })
-        return true;
-    });
-    return filter_by_target_owned;
+    return filter_by_agerange;
 };
 
 exports.securityIntersection = (sec1, sec2) => {
@@ -41,6 +30,7 @@ exports.targetDoesNotOwn = (target_sec, user_sec) => {
 
     return u_sec.filter(sec => !t_sec.includes(sec));
 };
+
 
 exports.filterKMostSimilarUsersSecurityBased = (target, users, k) => {
     let similarity_score = [];
--- src/services/SimilarityService.js
+++ src/services/SimilarityService.js
@@ -1,6 +1,6 @@
 const tf = require('@tensorflow/tfjs');
 
-    // Calculates the cosine similarity of two vectors
+// Calculates the cosine similarity of two vectors
 exports.cosine_similarity = (v1, v2) => {
     const t1 = tf.tensor(v1);
     const t2 = tf.tensor(v2);
@@ -13,6 +13,7 @@ exports.cosine_similarity = (v1, v2) => {
     return tf.div(scalar_prod_t1_t2, tf.mul(t1_norm, t2_norm)).arraySync();
 };
 
+// not used yet but maybe used in the future if we get information about price
 // Calculates the pearson similarity of two vectors.
 exports.pearson_similarity = (v1, v2) => {
     let t1 = tf.tensor(v1);
