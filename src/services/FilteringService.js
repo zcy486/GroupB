2dc578f57210cc65daad9eb38ac90fedb45a0384
Ludwig Dickmanns <dickmanns@fortiss.org>
2019-10-20 12:11:33 +0200

    Added user filtering and security intersection calculations to FilteringService.

--- src/services/FilteringService.js
+++ src/services/FilteringService.js
@@ -1,3 +1,5 @@
+const ss = require('./SimilarityService');
+
 //on condition that we have information of user's age range
 //filter the users first by age range, then by the target user's owned securities
 exports.filteringForVoting = function(target, users){
@@ -12,7 +14,61 @@ exports.filteringForVoting = function(target, users){
         return true;
     });
     return filter_by_target_owned;
-}
+};
+
+exports.securityIntersection = (sec1, sec2) => {
+    let s1 = [];
+    let s2 = [];
+    sec1.forEach(sec => {
+        s1.push(sec.isin);
+    });
+    sec2.forEach(sec => {
+        s2.push(sec.isin);
+    });
+
+    return s1.filter(sec => b.has(sec));
+};
+
+exports.filterKMostSimilarUsersSecurityBased = (target, users, k) => {
+    let similarity_score = [];
+    let target_sec = target.ownedSec;
+    users.forEach(user => {
+        let user_sec = user.ownedSec;
+        let intersection = this.securityIntersection(target_sec, user_sec);
+        let target_amounts = [];
+        let user_amounts = [];
+        intersection.forEach(i_isin => {
+            target_sec.forEach(sec => {
+                if (sec.isin === i_isin) target_amounts.push(sec.quantity);
+            });
+            user_sec.forEach(sec => {
+                if (sec.isin === i_isin) user_amounts.push(sec.quantity);
+            });
+        });
+        let sim = ss.cosine_similarity(target_amounts, user_amounts);
+        similarity_score.push({'user' : user, 'sim' : sim});
+    });
+
+    similarity_score.sort((a, b) => b.sim - a.sim);
+    similarity_score.slice(0, k);
+    similarity_score.map(e => e.user);
+    return similarity_score;
+};
+
+exports.filterKMostSimilarUsersPreferenceBased = (target, users, k) => {
+    let similarity_score = [];
+    let target_vector = target.uservector;
+    users.forEach(user => {
+        let user_vector = user.uservector;
+        let sim = ss.cosine_similarity(target_vector, user_vector);
+        similarity_score.push({'user' : user, 'sim' : sim});
+    });
+
+    similarity_score.sort((a, b) => b.sim - a.sim);
+    similarity_score.slice(0, k);
+    similarity_score.map(e => e.user);
+    return similarity_score;
+};
 
 //you may add more filtering methods here
 //TODO
