5d14d94cfec205651309930e3ebd085de61af134
Ludwig Dickmanns <dickmanns@fortiss.org>
2019-10-20 10:50:45 +0200

    Added pearson similarity function.

--- src/services/SimilarityService.js
+++ src/services/SimilarityService.js
@@ -12,3 +12,16 @@ exports.cosine_similarity = (v1, v2) => {
 
     return tf.div(scalar_prod_t1_t2, tf.mul(t1_norm, t2_norm)).arraySync();
 };
+
+// Calculates the pearson similarity of two vectors.
+exports.pearson_similarity = (v1, v2) => {
+    let t1 = tf.tensor(v1);
+    let t2 = tf.tensor(v2);
+
+    // Mean centered tensors of t1 and t2.
+    // This means the mean of all entries is substracted from each entry.
+    let mean_centered1 = tf.sub(t1, t1.mean());
+    let mean_centered2 = tf.sub(t2, t2.mean());
+
+    return this.cosine_similarity(mean_centered1.arraySync(), mean_centered2.arraySync())
+};
